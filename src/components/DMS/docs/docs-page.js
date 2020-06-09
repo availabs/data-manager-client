533cd378523f49ae1bf1259e4c898a01ddcacdc0
Eric Conklin <eric.s.conklin@gmail.com>
2020-06-09 16:20:20 -0400

    stuff

--- src/components/DMS/docs/docs-page.js
+++ src/components/DMS/docs/docs-page.js
@@ -5,6 +5,7 @@ import { Title } from "../components/parts"
 export default ({ title, chapter, body, footer, ...props }) =>
   <div className="max-w-md">
     <Title>{ title }</Title>
+    <div>{ chapter }</div>
     <div>{ body }</div>
     <div>{ footer }</div>
   </div>
