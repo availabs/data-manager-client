255e32d1feee22e599b8365b07964c97888d8e9d
Eric Conklin <eric.s.conklin@gmail.com>
2020-10-21 12:54:35 -0400

    stuff

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -20,17 +20,20 @@ const AuthConfig = {
 // Adds user to group (must have auth level 0 in all projects) instead of creating a request that must be accepted by admin.
       props: { addToGroup: "123" }
     },
-    { type: "ams-profile" },
     { type: "ams-verify-request" }, // This is only required when addToGroup prop is included with ams-signup
     { type: "ams-verify-email" }, // This is only required when addToGroup prop is not included with ams-signup
-    { type: "ams-set-password" }, // This is only required when addToGroup prop is not included with ams-signup
+
+    { type: "ams-set-password" },
+
+    { type: "ams-profile" },
     { type: "ams-accept-invite" },
 
     { type: "ams-project-management",
 // @props.authLevel
 // Optional prop. This prop can be applied to any AMS child.
 // If set, users must have equal or higher authLevel to view this page.
-      props: { authLevel: 5 }
+// Defaults to 5 for "ams-project-management."
+      // props: { authLevel: 5 }
     }
   ]
 }
